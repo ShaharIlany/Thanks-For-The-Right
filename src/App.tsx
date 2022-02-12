1b5895c3615b44b16aaf107e1ae99845dd64306e
Shahar Ilany <shahar.ilany@gmail.com>
2022-02-13 00:25:01 +0200

    create application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import logo from './assets/logo.jpeg'
 
 function App() {
+
+  const LOCAL_STORE_KEY = "lastClick"
+
+  const [alreadyClicked, setAlreadtClicked] = useState(0)
+
+  const [resetClicks, setResetClicks] = useState(0)
+
+  const getToday = () => {
+    let today = new Date();
+    return today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  }
+
+  const clicked = () => {
+    if (!alreadyClicked) {
+      localStorage.setItem(LOCAL_STORE_KEY, getToday())
+      setAlreadtClicked(1)
+    }else {
+      if (resetClicks == 10) {
+        setResetClicks(0)
+        localStorage.setItem(LOCAL_STORE_KEY, "")
+      setAlreadtClicked(0)
+      } else {
+        setResetClicks(resetClicks + 1)
+      }
+    }
+  }
+
+  useEffect(() => {
+    let localStore = localStorage.getItem(LOCAL_STORE_KEY) ?? ""
+    
+    if (localStore == getToday()) { 
+      setAlreadtClicked(2)
+    }
+
+  }, [])
+
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="mainFrame" style={{
+        width: '100vw',
+        height: '80vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <div style={{
+          textAlign: 'center',
+          alignItems: 'center',
+          direction: 'rtl'
+        }}>
+          {
+          alreadyClicked == 2 ? (
+            
+      <h1>כבר הודית על הזכות היום! בוא שוב להודות מחר
+      <br></br>
+      <br></br>
+      </h1>
+          ) :
+          alreadyClicked == 1 ?
+          (
+            
+            <h1>תודה שהודית על הזכות היוום! יום נעים☺️
+            <br></br>
+            <br></br>
+            </h1>
+                ) : (
+            
+            <h1>כבר הודית על הזכות היום?
+            <br></br>
+            <br></br>
+              לחץ כאן כדי להודות על הזכות
+            <br></br>
+            <br></br>
+      
+            </h1>
+                )
+        }
+            <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+
+        <div id="Button" onClick={clicked} style={{
+          height: '250px',
+          width: '250px',
+          backgroundColor: 'white',
+          borderRadius: '50%',
+          cursor: 'pointer',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+        }}>
+          <img src={logo} style={{
+            width: '175px'
+          }}></img>
+        </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
